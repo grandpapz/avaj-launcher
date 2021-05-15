b1eb10ebd08b901febda3ea11cea61ce15dd2a54
grandpapz <wartos@mail.ru>
2021-05-15 16:45:42 +0300

    Write Helicopter class. Need to write Simulator

--- src/ru/school21/lelida/avaj_launcher/WeatherProvider.java
+++ src/ru/school21/lelida/avaj_launcher/WeatherProvider.java
@@ -9,29 +9,19 @@ public class WeatherProvider {
     private static WeatherProvider weatherProvider;
     private static String[] weather = {"SUN", "RAIN", "FOG", "SNOW"};
 
-    private WeatherProvider(){}
+    private WeatherProvider() {
+    }
 
-    public static WeatherProvider getProvider(){
-        if (weatherProvider == null){
+    public static WeatherProvider getProvider() {
+        if (weatherProvider == null) {
             weatherProvider = new WeatherProvider();
         }
         return weatherProvider;
     }
 
-    public String getCurrentWeather(Coordinates coordinates){
+    public String getCurrentWeather(Coordinates coordinates) {
         int seed = 21 * (coordinates.getHeight() + coordinates.getLatitude() + coordinates.getLongitude());
 
         return weather[seed % 4];
     }
-
-//    public static void main(String[] args){
-//        Coordinates coordinates = new Coordinates(1, 1, 1);
-//        Coordinates coordinates2 = new Coordinates(2, 2, 2);
-//        Coordinates coordinates3 = new Coordinates(3, 3, 3);
-//        Coordinates coordinates4 = new Coordinates(4, 4, 4);
-//        Coordinates[] cord = {coordinates3, coordinates2, coordinates4, coordinates};
-//        for (Coordinates cords : cord) {
-//            System.out.println(WeatherProvider.weatherProvider.getCurrentWeather(cords));
-//        }
-//    }
 }
--- src/ru/school21/lelida/avaj_launcher/aircrafts/Helicopter.java
+++ src/ru/school21/lelida/avaj_launcher/aircrafts/Helicopter.java
@@ -2,6 +2,8 @@ package ru.school21.lelida.avaj_launcher.aircrafts;
 
 import ru.school21.lelida.avaj_launcher.WeatherTower;
 
+import java.util.HashMap;
+
 public class Helicopter extends Aircraft implements Flyable {
 
     private WeatherTower weatherTower;
@@ -12,13 +14,42 @@ public class Helicopter extends Aircraft implements Flyable {
 
     @Override
     public void updateConditions() {
+        String currentWeather = weatherTower.getWeather(this.coordinates);
 
+        switch (currentWeather){
+            case "SUN": this.coordinates = new Coordinates(coordinates.getLongitude() + 10,
+                    coordinates.getLatitude(), coordinates.getHeight() + 2 > 100 ? 100 : coordinates.getHeight() + 2);
+            break;
+            case "RAIN": this.coordinates = new Coordinates(coordinates.getLongitude() + 5,
+                    coordinates.getLatitude(), coordinates.getHeight());
+                break;
+            case "FOG": this.coordinates = new Coordinates(coordinates.getLongitude() + 1,
+                    coordinates.getLatitude(), coordinates.getHeight());
+                break;
+            case "SNOW": this.coordinates = new Coordinates(coordinates.getLongitude(),
+                    coordinates.getLatitude(), coordinates.getHeight() - 12);
+                break;
+            default: break;
+        }
+
+        HashMap<String, String> message = new HashMap<>();
+        message.put("SUN", "Ohhh Sunny weather is pretty fine!");
+        message.put("RAIN", "Rain rain go away!");
+        message.put("FOG", "fog! i can see nothing!");
+        message.put("SNOW", "Let it snow!");
+
+        //need some filewriter System.out.println("Helicopter#" + this.name + "(" + this.id + "):" + message.get(currentWeather));
+        if (this.coordinates.getHeight() <= 0){
+            //need some filewriter   System.out.println("Helicopter#" + this.name + "(" + this.id + "): grounded");
+            this.weatherTower.unregister(this);
+            //need some filewriter   System.out.println("Tower says: Helicopter#" + this.name + "(" + this.id + ")" + "unregistered to weather tower.");
+        }
     }
 
     @Override
     public void registerTower(WeatherTower weatherTower) {
         this.weatherTower = weatherTower;
         this.weatherTower.register(this);
-        System.out.println("Tower says: Helicopter#" + this.name + "(" + this.id + ")" + "registered to weather tower.");
+        //need some filewriter ("Tower says: Helicopter#" + this.name + "(" + this.id + ")" + "registered to weather tower.");
     }
 }
